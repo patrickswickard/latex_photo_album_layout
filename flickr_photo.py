7d3c60ea85291f7783bd979da0f235442119f8f6
swickape <patrick.swickard@amplyfigroup.com>
2023-03-25 13:50:12 -0400

    progress on appropriately laying out pages by orientation

--- flickr_photo.py
+++ flickr_photo.py
@@ -9,3 +9,38 @@ class Photo:
       self.orientation = 'P'
     else:
       self.orientation = 'L'
+
+class Page:
+    def __init__(self):
+      self.photo_list = []
+      self.layout = ''
+
+    def add_photo(self,photo):
+      self.photo_list.append(photo)
+      self.layout += photo.orientation
+
+    def canfit_l(self):
+      if (self.layout == ''):
+        return True
+      if (self.layout == 'L'):
+        return True
+      if (self.layout == 'P'):
+        return True
+      if (self.layout == 'PP'):
+        return True
+      return False
+
+    def canfit_p(self):
+      if (self.layout == ''):
+        return True
+      if (self.layout == 'L'):
+        return True
+      if (self.layout == 'LP'):
+        return True
+      if (self.layout == 'P'):
+        return True
+      if (self.layout == 'PP'):
+        return True
+      if (self.layout == 'PPP'):
+        return True
+      return False
--- parse_flickr_api_json.py
+++ parse_flickr_api_json.py
@@ -20,46 +20,78 @@ def parse_file():
     thatlist.sort(key = lambda x: thathash[x]['title'])
     for entry in thatlist:
       print(thathash[entry]['title'])
-    print('**********')
+    #print('**********')
     album_title = thathash[thatlist[0]]['title']
-    print(album_title)
+    #print(album_title)
+    # THIS IS HARD-CODED SO WE JUST DO ONE
     album_entries = thathash[thatlist[0]]['photoset_hash']
-    print(album_entries)
-    print(album_entries[0].keys())
-    caption = album_entries[0]['title']
-    print(caption)
-    id = album_entries[0]['id']
-    print(id)
-    url = thishash[id]
-    print(url)
-    url2 = album_entries[0]['url']
-    print(url2)
-    # well crap, I thought I had dimensions in one of these hashes, need to figure out that next...
-    print('***********************')
-    print('FLICKR ORIGINAL INFO')
-    print('***********************')
-    original_id = theotherhash[id]['id']
-    original_caption = theotherhash[id]['title']
-    original_url = theotherhash[id]['url_o']
-    width = theotherhash[id]['width_o']
-    height = theotherhash[id]['height_o']
-    print(original_id)
-    print(original_url)
-    print(original_caption)
-    print(width)
-    print(height)
-    print(theotherhash[id])
-    thisphoto = flickr_photo.Photo(id,original_url,original_caption,width,height)
-    print(thisphoto.id)
-    print(thisphoto.location)
-    print(thisphoto.caption)
-    print(thisphoto.width)
-    print(thisphoto.height)
-    print(thisphoto.orientation)
+    #print(album_entries)
+    photo_list = []
+    for thisphoto in album_entries:
+      #print(album_entries[0].keys())
+      caption = album_entries[0]['title']
+      #print(caption)
+      #id = album_entries[0]['id']
+      id = thisphoto['id']
+      #print(id)
+      url = thishash[id]
+      #print(url)
+      url2 = album_entries[0]['url']
+      #print(url2)
+      # well crap, I thought I had dimensions in one of these hashes, need to figure out that next...
+      #print('***********************')
+      #print('FLICKR ORIGINAL INFO')
+      #print('***********************')
+      original_id = theotherhash[id]['id']
+      original_caption = theotherhash[id]['title']
+      original_url = theotherhash[id]['url_o']
+      width = theotherhash[id]['width_o']
+      height = theotherhash[id]['height_o']
+      #print(original_id)
+      #print(original_url)
+      #print(original_caption)
+      #print(width)
+      #print(height)
+      #print(theotherhash[id])
+      #thisphoto1 = flickr_photo.Photo(id,original_url,original_caption,width,height)
+      thisphoto1 = flickr_photo.Photo(id,original_url,original_caption,width,height)
+      photo_list.append(thisphoto1)
+      #print(thisphoto1.id)
+      #print(thisphoto1.location)
+      #print(thisphoto1.caption)
+      #print(thisphoto1.width)
+      #print(thisphoto1.height)
+      print(thisphoto1.orientation)
     thisfile.close()
     thatfile.close()
     theotherfile.close()
 
+    print("****************")
+    book_list = []
+    current_page = flickr_photo.Page()
+    for thisphoto in photo_list:
+      if (thisphoto.orientation == 'L') and (current_page.canfit_l()):
+        current_page.add_photo(thisphoto)
+        print(current_page.layout)
+        print(current_page.canfit_l())
+      elif (thisphoto.orientation == 'P') and (current_page.canfit_p()):
+        current_page.add_photo(thisphoto)
+        print(current_page.layout)
+        print(current_page.canfit_p())
+      else:
+        book_list.append(current_page)
+        current_page = flickr_photo.Page()
+        current_page.add_photo(thisphoto)
+      # add final page
+      book_list.append(current_page)
+
+    print('$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$')
+    onepage = book_list[0]
+    for thisphoto in onepage.photo_list:
+      x = 1
+      #print(thisphoto.orientation)
+    for thispage in book_list:
+        print(thispage.layout)
 
 # Press the green button in the gutter to run the script.
 if __name__ == '__main__':
