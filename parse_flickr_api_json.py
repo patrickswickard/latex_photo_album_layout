beeddfb15852ce61fc2c5aff24353fe32ee5705a
swickape <patrick.swickard@amplyfigroup.com>
2023-04-03 13:33:35 -0400

    checkin vol 21

--- parse_flickr_api_json.py
+++ parse_flickr_api_json.py
@@ -107,8 +107,10 @@ def make_one_multi_section_book(all_sections):
     #section_list = section_list[69:73]
     #book_filename = 'ctr_019'
     #section_list = section_list[73:76]
-    book_filename = 'ctr_020'
-    section_list = section_list[76:103]
+    #book_filename = 'ctr_020'
+    #section_list = section_list[76:103]
+    book_filename = 'ctr_021'
+    section_list = section_list[103:115]
     total_pages = 0
     for this_section in section_list:
       print(this_section.title)
