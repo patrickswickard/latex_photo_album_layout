564ee89ed5eb924174d5f34455f9aebae07cecb3
Patrick Swickard <swickape@hotmail.com>
2023-09-21 10:57:18 -0400

    generic poem checkin

--- generic_poems.py
+++ generic_poems.py
@@ -14,8 +14,8 @@ generic_poem_body_2 = [
   'LINE2-4',
   'LINE2-5',
 ]
-generic_poem_image1 = 'cache/GENERIC/GENERIC1.jpg'
-generic_poem_image2 = 'cache/GENERIC/GENERIC2.jpg'
+generic_poem_image_1 = 'cache/GENERIC/GENERIC1.jpg'
+generic_poem_image_2 = 'cache/GENERIC/GENERIC2.jpg'
 
 class Poem:
   def __init__(self):
@@ -23,4 +23,21 @@ class Poem:
     self.body = []
     self.image = 'cache/GENERIC/noimage.jpg'
 
-poem1 = Poem()
+poem_1 = Poem()
+poem_2 = Poem()
+
+poem_1.title = generic_poem_title_1
+poem_2.title = generic_poem_title_2
+
+poem_1.body = generic_poem_body_1
+poem_2.body = generic_poem_body_2
+
+poem_1.image = generic_poem_image_1
+poem_2.image = generic_poem_image_2
+
+poem_list = []
+poem_list.append(poem_1)
+poem_list.append(poem_2)
+
+for thispoem in poem_list:
+  print(thispoem.title)
