3fc169327869d3a31a0411ae4976adda1214e5a6
Patrick Swickard <swickape@hotmail.com>
2024-01-02 12:27:46 -0500

    pylint changes

--- make_one_big_file.py
+++ make_one_big_file.py
@@ -1,37 +1,34 @@
-# This )is a sample Python script.
-
-# Press Shift+F10 to execute it or replace it with your code.
-# Press Double Shift to search everywhere for classes, files, tool windows, actions, and settings.
-
+"""Make one big file out of previously loaded data"""
 import json
 
-# this is just a utility script that was created to make one big file out of previously downloaded flickr metadata
+# this is just a utility script that was created
+# to make one big file out of previously downloaded flickr metadata
 def make_one_big_file():
-    bighash = {}
-    filenamelist = []
-    for i in range(1,65):
-      if i < 10:
-        # this won't work out of context due to hard-coded prefix
-        filename = "/home/swickape/Pictures/flickr/photos/0" + str(i) + ".txt"
-      else:
-        # this won't work out of context due to hard-coded prefix
-        filename = "/home/swickape/Pictures/flickr/photos/" + str(i) + ".txt"
-      filenamelist.append(filename)
-    for thisfilename in filenamelist:
-      thisfile = open(thisfilename, 'r')
-      thishash = json.load(thisfile)
-      photolist = thishash['photos']['photo']
-      for photo in photolist:
-          id = photo['id']
-          bighash[id] = photo
-      thisfile.close()
-    all_info_file = open('all_info_file.json','w')
-    all_info_file.write(json.dumps(bighash))
-    all_info_file.close()
-
+  """Make one big file out of previously loaded data"""
+  bighash = {}
+  filenamelist = []
+  for i in range(1,65):
+    if i < 10:
+      # this won't work out of context due to hard-coded prefix
+      filename = "/home/swickape/Pictures/flickr/photos/0" + str(i) + ".txt"
+    else:
+      # this won't work out of context due to hard-coded prefix
+      filename = "/home/swickape/Pictures/flickr/photos/" + str(i) + ".txt"
+    filenamelist.append(filename)
+  for thisfilename in filenamelist:
+    with open(thisfilename, 'r', encoding='utf-8') as myinfile:
+#    thisfile = open(thisfilename, 'r')
+      thishash = json.load(myinfile)
+    photolist = thishash['photos']['photo']
+    for photo in photolist:
+      thisid = photo['id']
+      bighash[thisid] = photo
+#    thisfile.close()
+  with open('all_info_file.json','w',encoding='utf-8') as myoutfile:
+    myoutfile.write(json.dumps(bighash))
 
 # Press the green button in the gutter to run the script.
 if __name__ == '__main__':
-    make_one_big_file()
+  make_one_big_file()
 
 # See PyCharm help at https://www.jetbrains.com/help/pycharm/
