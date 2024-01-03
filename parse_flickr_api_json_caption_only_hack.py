93ad219cb7525b9b0f9e413a0ac11d4d1f866bba
Patrick Swickard <swickape@hotmail.com>
2024-01-03 10:55:37 -0500

    pylint encoding changes

--- parse_flickr_api_json_caption_only_hack.py
+++ parse_flickr_api_json_caption_only_hack.py
@@ -13,7 +13,7 @@ def parse_file():
   # this file contains json hash keyed on album id
   # entries are an album title and sequential list of photos
   # with id caption and url
-  album_file = open('allalbumswithurls4.json', 'r')
+  album_file = open('allalbumswithurls4.json', 'r', encoding='utf-8')
   album_hash = json.load(album_file)
   album_file.close()
   # this file contains a json hash keyed on photo id
@@ -22,7 +22,7 @@ def parse_file():
   # the owner (author) id can be obtained as well which could allow for
   # constructing some of the hard-coded urls used in the code if desired
   # e.g. owner has id 99753978@N03
-  all_info_file = open('all_info_file.json', 'r')
+  all_info_file = open('all_info_file.json', 'r', encoding='utf-8')
   all_info_hash = json.load(all_info_file)
   all_info_file.close()
   album_code_list = []
@@ -176,7 +176,7 @@ def make_one_multi_section_book(all_sections):
     total_pages += len(this_section.page_list)
     print("Pages in book so far: " + str(total_pages))
   output_filename = 'texfiles2/' + book_filename + '.tex'
-  output_file = open(output_filename, 'w')
+  output_file = open(output_filename, 'w', encoding='utf-8')
   this_book = flickr_photo_caption_only_hack.Book(output_file)
   this_book.title = ''
   this_book.author = ''
@@ -192,7 +192,7 @@ def make_all_single_section_books(all_sections):
     # for now we are restricting books to one section...
     section_list = [this_section]
     output_filename = 'texfiles/' + this_section.title + '.tex'
-    output_file = open(output_filename, 'w')
+    output_file = open(output_filename, 'w', encoding='utf-8')
     this_book = flickr_photo_caption_only_hack.Book(output_file)
     this_book.title = this_section.title
     this_book.author = this_section.author
