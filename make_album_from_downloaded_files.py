e37c6a25beb24e2b125149a26ce68649862040a3
Patrick Swickard <swickape@hotmail.com>
2024-01-04 10:41:48 -0500

    get rid of unused variables

--- make_album_from_downloaded_files.py
+++ make_album_from_downloaded_files.py
@@ -128,8 +128,6 @@ def get_page_list(photo_list):
 def get_section(this_album,page_list):
   this_section = flickr_photo.Section()
   for thispage in page_list:
-    layout = thispage.layout
-    photo_list = thispage.photo_list
     this_section.add_page(thispage)
   this_section.title = this_album.title
   this_section.author = this_album.author
